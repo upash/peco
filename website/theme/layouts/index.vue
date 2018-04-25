1c71394ebcbe0f110c32c1e3ba77f1021ffc1bb4
EGOIST <0x142857@gmail.com>
2018-04-25 18:25:36 +0800

    Initial website layout

---
+++ website/theme/layouts/index.vue
@@ -0,0 +1,71 @@
+<template>
+  <div>
+    <div class="hero">
+      <div class="container">
+        <h1 class="title">{{ title }}</h1>
+        <h2 class="description">{{ description }}</h2>
+      </div>
+    </div>
+    <div class="container">
+      <Features :features="page.attributes.features" />
+    </div>
+  </div>
+</template>
+
+<script>
+import Features from '@theme/components/Features.vue'
+import { title, description } from '@site-meta'
+
+export default {
+  props: ['page'],
+
+  components: {
+    Features
+  },
+
+  head: {
+    title,
+    meta: [
+      {
+        name: 'description',
+        content: description
+      }
+    ]
+  },
+
+  data() {
+    return {
+      title,
+      description
+    }
+  }
+}
+</script>
+
+<style src="@theme/styles/main.styl" lang="stylus"></style>
+
+<style lang="stylus" scoped>
+.container {
+  max-width: 900px;
+  margin: 0 auto;
+}
+
+.hero {
+  background: #f9f9f9;
+  padding: 100px 0;
+  margin-bottom: 100px;
+}
+
+.title {
+  margin: 0;
+  font-weight: 300;
+  font-size: 3rem;
+}
+
+.description {
+  margin: 0;
+  font-weight: 300;
+  color: #797272;
+}
+</style>
+
