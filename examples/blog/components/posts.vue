cfa3ad262eddc7c3fce012863e356b7ba3fbe312
EGOIST <0x142857@gmail.com>
2018-04-26 18:37:15 +0800

    Custom routes

---
+++ examples/blog/components/posts.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="posts">
+    <div class="post" v-for="post in posts">
+      <h2>{{ post.title }}</h2>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['posts']
+}
+</script>
--- examples/blog/peco.config.js
+++ examples/blog/peco.config.js
@@ -1,7 +1,24 @@
 const axios = require('axios')
 
+const routes = [
+  {
+    component: 'components/posts.vue',
+    path: '/posts',
+    props: async () => {
+      const { data } = await axios.get('https://jsonplaceholder.typicode.com/posts')
+      return {
+        posts: data
+      }
+    }
+  }
+]
+
 module.exports = {
   title: 'Peco',
   description: 'Humbly powered by Peco!!!',
-  permalink: ':year/:month/:day/:slug.html'
+  permalink: ':year/:month/:day/:slug.html',
+
+  plugins: [
+    ['../../lib/plugins/routes', routes]
+  ]
 }
