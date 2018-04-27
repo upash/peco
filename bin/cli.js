01e9ab3ef0e33e3b23e8bace8302beef4b755f89
EGOIST <0x142857@gmail.com>
2018-04-27 18:25:54 +0800

    support node 8

--- bin/cli.js
+++ bin/cli.js
@@ -3,7 +3,9 @@ if (parseInt(process.versions.node, 10) < 8) {
   const chalk = require('chalk')
   console.error(
     chalk.red(
-      `Peco requires Node.js version >= 8, please upgrade!\nCheck out ${chalk.underline('https://nodejs.org')}`
+      `Peco requires Node.js version >= 8, please upgrade!\nCheck out ${chalk.underline(
+        'https://nodejs.org'
+      )}`
     )
   )
   process.exit(1)
@@ -14,21 +16,13 @@ const cac = require('cac').default
 const cli = cac()
 
 cli.command('dev', 'Develop website locally', (input, flags) => {
-  const options = {
-    ...flags,
-    baseDir: input[0]
-  }
-  const app = require('../lib')(options)
+  const app = require('../lib')(Object.assign({ baseDir: input[0] }, flags))
 
   return app.dev()
 })
 
 cli.command('build', 'Build website to static HTML files', (input, flags) => {
-  const options = {
-    ...flags,
-    baseDir: input[0]
-  }
-  const app = require('../lib')(options)
+  const app = require('../lib')(Object.assign({ baseDir: input[0] }, flags))
 
   return app.build()
 })
